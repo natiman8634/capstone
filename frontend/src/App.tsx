9ec6c62b6b19d622c8f550045d1fb9bfa21a3b6c
natiman8634 <natiman863400@gmail.com>
2026-02-11 15:20:12 -0800

    second change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+// this is a comment to test the hot module replacement (HMR) feature of Vite. When you edit this file and save it, the changes will be reflected in the browser without a full page reload. This allows for a faster development experience.
+  
+return (
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
